c8ea66681825a816ed7eeda653f03a4470930b1a
unknown <117663776+devaguava@users.noreply.github.com>
2023-03-14 17:18:38 +0700

    menambahkan class PosisiKaraterGame

--- mod4_1302213111/Program.cs
+++ mod4_1302213111/Program.cs
@@ -1,5 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Security.Cryptography.X509Certificates;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 public class KodeBuah
 {
     public enum NamaBuah { Apel, Aprikot, Alpukat, Pisang, Paprika, Blackberry, Ceri, Kelapa, Jagung, Kurma, Durian, Anggur, Melon, Semangka }
@@ -10,10 +13,61 @@ public class KodeBuah
         return kode_buah[(int)kode];
     }
 
-    public static void Main()
+    public class PosisiKarakterGame
     {
-        KodeBuah kode_buah = new KodeBuah();
-        string getKodeBuah = KodeBuah.getKodeBuah(KodeBuah.NamaBuah.Apel);
-        Console.WriteLine("Kode Buah " + NamaBuah.Apel+" adalah "+getKodeBuah+Environment.NewLine);
+        enum State { Berdiri, Jongkok, Terbang, Tengkurap }
+        enum tombol { TombolS, TombolW, TombolX }
+
+        public static void Main()
+        {
+            //kode buah
+            KodeBuah kode_buah = new KodeBuah();
+            string getKodeBuah = KodeBuah.getKodeBuah(KodeBuah.NamaBuah.Apel); // misal get kode buah apel
+            Console.WriteLine("Kode Buah " + NamaBuah.Apel + " adalah " + getKodeBuah + Environment.NewLine);
+
+            // posisi karakter game
+            State currentstate = State.Berdiri;
+            string[] screenName = { "Posisi Berdiri", "Posisi Jongkok", "Posisi Terbang", "Posisi Tengkurap" };
+            while (currentstate != null)
+            {
+                Console.WriteLine(screenName[(int)currentstate]);
+                Console.WriteLine("Enter Command : ");
+
+                string command = Console.ReadLine();
+                switch (currentstate)
+                {
+                    case State.Berdiri:
+                        if (command == "TombolS")
+                            Console.WriteLine("tombol arah bawah ditekan");
+                        else if (command == "TombolW")
+                            Console.WriteLine("tombol arah atas ditekan");
+                        break;
+
+                    case State.Jongkok:
+                        if (command == "TombolS")
+                            Console.WriteLine("tombol arah bawah ditekan");
+                        else if (command == "TombolW")
+                            Console.WriteLine("tombol arah atas ditekan");
+                        break;
+
+                    case State.Terbang:
+                        if (command == "TombolS")
+                            Console.WriteLine("tombol arah bawah ditekan");
+                        else if (command == "TombolW")
+                            Console.WriteLine("tombol arah atas ditekan");
+                        break;
+
+                    case State.Tengkurap:
+                        if (command == "TombolS")
+                            Console.WriteLine("tombol arah bawah ditekan");
+                        break;
+                }
+            }
     }
+
+    }
+
+    
 }
+
+
